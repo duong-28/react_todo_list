65550b3afefe2b9630efab1ad38345ab0df1cb92
Brooklin C <brooklincharlton1@gmail.com>
2024-11-19 19:50:47 +1000

    Over to you champion

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,14 @@
+import { useState } from 'react';
 import TodoItem from './components/TodoItem';
 
 function App() {
+
+  const [todos, setTodos] = useState([
+    "Learn about React",
+    "Meet a friend for lunch",
+    "Build a really cool todo App",
+  ]);
+
   return (
     <div>
       <h1>My To Do List</h1>
